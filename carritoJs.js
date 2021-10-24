0a87767fc6a7571a2a724be6defc14a4922215b6
IGaitanM <ivanovef@gmail.com>
2021-10-24 13:16:37 +0200

    corrijo fallos en funcion anadir

--- carritoJs.js
+++ carritoJs.js
@@ -1,49 +1,51 @@
 var nombre;
 var precio;
 var unidades;
+var botonAñadir;
 var artCarrito;
 var precioTot;
 var pago;
 
-
-function anadir() {
-    if (!nombre.value=="" || precio.value=="" || unidades.value=="" ){
-        precio.value=parseFloat.precio.value;
-        unidades.value=parseInt.unidades.value;
-        precioTot.value=precio.value*unidades.value;
-        artCarrito.value+=nombre.value + " ,";
-    }else
-        alert("Falta información del producto")
-}  
-
-
-function cargarPago (){
-    if (pago.value=="Seleccione"){
-        capaTarjeta.tarjeta.style.display="none";
-        capaEfectivo.tarjeta.style.display="none";
-    }else if (pago.value=="Tarjeta"){
-        capaTarjeta.tarjeta.style.display="block";
-        capaEfectivo.tarjeta.style.display="none";
-    }else{
-        capaTarjeta.tarjeta.style.display="none";
-        capaEfectivo.tarjeta.style.display="block";
-    } 
-}
-
 function inicializa(){
-    nombre = document.formulario.nombre
+    nombre = document.formulario.nombre;
     precio = document.formulario.precio;
     unidades = document.formulario.unidades;
+    botonAñadir = document.formulario.anadir;
     artCarrito = document.formulario.artCarrito;
     precioTot = document.formulario.precioTot;
     pago = document.formulario.formaPago;
 
-  
+  }
 
 function eventos(){
-    document.formulario.anadir.addEventListener("click",anadir);
+    botonAñadir.addEventListener("click",anadir)
 
 }    
+
+function anadir() {
+    if (!nombre.value=="" && !precio.value=="" && !unidades.value=="" ){
+        artCarrito.value += nombre.value + " ,";
+        precioTot.value += precio.value * unidades.value;
+        nombre.value = "";
+        precio.value = "";
+        unidades.value = 1;
+    }else
+        alert("Falta información del producto")
+}  
+
+function cargarPago (){
+    if (pago.value=="Seleccione"){
+        capaTarjeta.style.display="none";
+        capaEfectivo.style.display="none";
+    }else if (pago.value=="Tarjeta"){
+        capaTarjeta.style.display="block";
+        capaEfectivo.style.display="none";
+    }else{
+        capaTarjeta.style.display="none";
+        capaEfectivo.style.display="block";
+    } 
+}
+
 
 window.onload=function(){
     inicializa();
@@ -52,4 +54,5 @@ window.onload=function(){
     eventos();
 }
 
-}
+
+
